7a5b650efa3735afafb27d017548df9e3f7a81e5
Toni Palanques <tpalanques@gmail.com>
2022-11-08 20:06:55 +0100

    Add feature to clean all vendor files

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,31 +4,40 @@ source system.sh
 
 system.setOriginalPath
 
-function start(){
-    system.cdRootPath
-    docker-compose up --detach --force-recreate --remove-orphans --always-recreate-deps --build
-    system.cdOriginalPath
+function clean(){
+  system.cleanVendorPath
 }
 
-function stop(){
-    system.cdRootPath
-    sudo docker-compose down
-    system.cdOriginalPath
+function start() {
+  system.cdRootPath
+  docker-compose up --detach --force-recreate --remove-orphans --always-recreate-deps --build
+  system.cdOriginalPath
+}
+
+function stop() {
+  system.cdRootPath
+  sudo docker-compose down
+  system.cdOriginalPath
 }
 
 command=$1
 case $command in
-    restart)
-        stop
-        start
-    ;;
-    start)
-        start
-    ;;
-    stop)
-        stop
-    ;;
-    *)
-        echo "Command not found";
-    ;;
+rebuild)
+  stop
+  clean
+  start
+  ;;
+restart)
+  stop
+  start
+  ;;
+start)
+  start
+  ;;
+stop)
+  stop
+  ;;
+*)
+  echo "Command not found"
+  ;;
 esac
