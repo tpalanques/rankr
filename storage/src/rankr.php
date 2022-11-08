a20aea15e75cfd5e8ff04df1670c7ec68ec51543
Toni Palanques <tpalanques@gmail.com>
2022-11-08 20:08:28 +0100

    Show error message if autoload is not found

--- storage/src/rankr.php
+++ storage/src/rankr.php
@@ -1,12 +1,10 @@
 <?php
+declare (strict_types=1);
 //defines
 session_start();
 error_reporting(E_ALL);
 define('DIR_ROOT', '../');
-
 if (include_once DIR_ROOT . 'vendor/autoload.php') {
-    //$app = new Controller\Router();
-    //$app->route($_SERVER['REQUEST_URI']);ç
     echo "Service ready to rock";
 } else {
     echo "Can't start service, no autoloader found";
