2488fd01a6b4b09163b1b505cdae047e31e53e75
Toni Palanques <tpalanques@gmail.com>
2022-11-09 23:06:45 +0100

    Rename parameter variables to properties

--- storage/src/Controller/_Type/ViewableWithProperties.php
+++ storage/src/Controller/_Type/ViewableWithProperties.php
@@ -5,16 +5,32 @@ namespace Rankr\Controller\_Type;
 use Rankr\View\_Type\View;
 
 abstract class ViewableWithProperties extends Viewable {
-
+    private array $properties;
     /**
      * @param string $okView
      * @param string $koView
-     * @param array|null $parameters
-     * @param array|null $mandatoryParameters
+     * @param array|null $properties
+     * @param array|null $mandatoryProperties
      */
-    public function __construct(string $okView, string $koView, array $parameters = null, array $mandatoryParameters = null) {
-        $view = $this->propertiesSet($parameters, $mandatoryParameters) ? $okView : $koView;
+    public function __construct(string $okView, string $koView, array $properties = null, array $mandatoryProperties = null) {
+        $this->properties = $properties;
+        $view = $this->propertiesSet($properties, $mandatoryProperties) ? $okView : $koView;
         parent::__construct($view);
+    }
+
+    /**
+     * @param string $propertyName
+     * @return mixed|null
+     */
+    protected function getProperty(string $propertyName) {
+        return array_key_exists($propertyName, $this->properties) ? $this->properties[$propertyName] : null;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getProperties(): array {
+        return $this->properties;
     }
 
     /**
