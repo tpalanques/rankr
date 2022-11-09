f7d6201e5ed99d8202d754e08c53284452023c3c
Toni Palanques <tpalanques@gmail.com>
2022-11-09 22:59:50 +0100

    Rename method

--- storage/src/Controller/_Type/ViewableWithProperties.php
+++ storage/src/Controller/_Type/ViewableWithProperties.php
@@ -13,7 +13,7 @@ abstract class ViewableWithProperties extends Viewable {
      * @param array|null $mandatoryParameters
      */
     public function __construct(string $okView, string $koView, array $parameters = null, array $mandatoryParameters = null) {
-        $view = $this->parametersAreSet($parameters, $mandatoryParameters) ? $okView : $koView;
+        $view = $this->propertiesSet($parameters, $mandatoryParameters) ? $okView : $koView;
         parent::__construct($view);
     }
 
@@ -22,7 +22,7 @@ abstract class ViewableWithProperties extends Viewable {
      * @param array $parameters
      * @return bool
      */
-    private function parametersAreSet(array $array, array $parameters): bool {
+    private function propertiesSet(array $array, array $parameters): bool {
         foreach ($parameters as $parameter) {
             if (!array_key_exists($parameter, $array)) {
                 return false;
